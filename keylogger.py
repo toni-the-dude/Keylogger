ab54da6287854624960d38a133f2dbb7a61b29b2
anthonyw <anthonyw@Jatusthira.myguest.virtualbox.org>
2024-01-28 10:05:30 -0700

    Add key-stroke listener

--- keylogger.py
+++ keylogger.py
@@ -1,11 +1,12 @@
-import os
+import pynput
 
-pid = os.fork()
+from pynput.keyboard import Key, Listener
 
-print(pid)
+def on_press(key):
+    print("press")
 
-while True:
-    x = input()
-    print(pid)
-    
-    
+def on_release(key):
+    print("release")
+
+with Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
